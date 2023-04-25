23193e5bacfc36d2e2ba96dce06fbc67ccc8bce2
ricardoshiga <roppongi_forever@hotmail.com>
2023-04-25 15:33:19 -0300

    Ajustes finais do Menu

--- js/menu.js
+++ js/menu.js
@@ -5,4 +5,12 @@ const icone = document.querySelector(".icone");
 botao.addEventListener("click", function(event){
     event.preventDefault();
     linksMenu.classList.toggle("aberto");
+
+    /* Se a classe "aberto" estiver aplicada ao linkMenu, mude o texto/icone para "Fechar X".
+    Caso contrário, mostre o texto/icone "Menu &equiv;" */
+    if(linksMenu.classList.contains("aberto")){
+        icone.innerHTML = "Fechar &times";
+    } else {
+        icone.innerHTML = "Menu &equiv;"
+    }
 });
